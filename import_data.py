2342f22a46c87eebe99f32b2e19183992331afe8
Terry Chen <TerryChen@Xuhuis-MacBook-Pro.local>
2015-02-28 11:34:01 -0800

    change attributes to private

--- import_data.py
+++ import_data.py
@@ -1,37 +1,36 @@
 import os
 import pandas as pd
 
-folder = "Data"
+__folder = "Data"
 
 
-
-def read_file(fn):
+def __read_file(fn):
     print('Reading file: ' + fn + '...')
-    data_df = pd.read_csv(os.path.join(folder), fn)
+    data_df = pd.read_csv(os.path.join(__folder), fn)
     return data_df
 
 
 def get_outcome_df():
     fn = 'outcomes.csv'
-    outcome_df = read_file(fn)
+    outcome_df = __read_file(fn)
     return outcome_df
 
 
 def get_project_df():
     fn = 'projects.csv'
-    projects_df = read_file(fn)
+    projects_df = __read_file(fn)
     return projects_df
 
 
 def get_essays_df():
     fn = 'essays.csv'
-    essays_df = read_file(fn)
+    essays_df = __read_file(fn)
     return essays_df
 
 
 def get_resources_df():
     fn = 'resources.csv'
-    resources_df = read_file(fn)
+    resources_df = __read_file(fn)
     return resources_df
 
 
