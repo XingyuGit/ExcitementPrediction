117e3d0a512999c2da5883ebdc61c79c63cd5a4d
Xingyu Zhou <xingyuhit@gmail.com>
2015-02-28 20:55:39 -0800

    Merge branch 'master' into xingyu-zhou

--- import_data.py
+++ import_data.py
@@ -6,19 +6,29 @@ __folder = "Data"
 
 def __read_file(fn):
     print('Reading file: ' + fn + '...')
-    data_df = pd.read_csv(os.path.join(__folder), fn)
+    data_df = pd.read_csv(os.path.join(__folder, fn))
     return data_df
 
 
 def get_outcomes_df():
     fn = 'outcomes.csv'
     outcomes_df = __read_file(fn)
+    outcomes_df['y'] = 0
+    outcomes_df['y'][outcome_df['is_exciting'] == 't'] = 1
     return outcomes_df
 
 
 def get_projects_df():
     fn = 'projects.csv'
     projects_df = __read_file(fn)
+    # only data after 2010-4-1 will be used
+    # for complete model building, training data  is from 2010-4-1 to 2014-1-1 (exclusive)
+    # for optimization, use validation data to optimize performance
+    # for prediction, using testing data (after 2014-4-1)
+    projects_df['group'] = 'train'
+    projects_df['group'][projects_df['date_posted'] < '2010-04-01'] = 'none'
+    projects_df['group'][projects_df['date_posted'] >= '2013-01-01'] = 'valid'
+    projects_df['group'][projects_df['date_posted'] >= '2014-01-01'] = 'test'
     return projects_df
 
 
