0a05f11d6e00502ce38719afbf6df2c064c535c0
Xingyu Zhou <xingyuhit@gmail.com>
2015-03-01 19:45:11 -0800

    change columns name

--- feature_history.py
+++ feature_history.py
@@ -20,13 +20,13 @@ def __acc_cnt(df, list_of_vars, list_of_cnt_cols):
         df_sorted = df.sort([var, 'date_posted'])
         df_grouped = df.groupby(var)
 
-        acc_cnt_col = 'cum_cnt_by_' + var
+        acc_cnt_col = 'cum_cnt_for_' + var
         df_sorted[acc_cnt_col] = df_grouped['one'].cumsum() - df_sorted['one']
         df[acc_cnt_col] = df_sorted[acc_cnt_col].sort_index()
 
         for cnt_col in list_of_cnt_cols:
-            cumsum_col = cnt_col + '_acc_by_' + var
-            rate_col = cnt_col + '_rate_by_' + var
+            cumsum_col = cnt_col + '_acc_for_' + var
+            rate_col = cnt_col + '_rate_for_' + var
             df_prev_cumsum = df_grouped[cnt_col].cumsum() - df_sorted[cnt_col]
             df_prev_rate = df_prev_cumsum / df_sorted[acc_cnt_col]
             df_prev_rate[np.isinf(df_prev_rate)] = 0
@@ -37,11 +37,11 @@ def __acc_cnt(df, list_of_vars, list_of_cnt_cols):
 projects_df = get_projects_df()
 outcomes_df = get_outcomes_df()
 
-# list_of_columns = ['is_exciting', 'at_least_1_teacher_referred_donor', 'fully_funded',
-#     'at_least_1_green_donation', 'great_chat', 'three_or_more_non_teacher_referred_donors',
-#     'one_non_teacher_referred_donor_giving_100_plus', 'donation_from_thoughtful_donor']
+list_of_columns = ['is_exciting', 'at_least_1_teacher_referred_donor', 'fully_funded',
+    'at_least_1_green_donation', 'great_chat', 'three_or_more_non_teacher_referred_donors',
+    'one_non_teacher_referred_donor_giving_100_plus', 'donation_from_thoughtful_donor']
 
-list_of_columns = ['is_exciting']
+# list_of_columns = ['is_exciting']
 list_of_cnt_cols = [x + '_cnt' for x in list_of_columns]
 list_of_vars = ['teacher_acctid']
 
@@ -53,7 +53,7 @@ print "elapsed time: " + str((time() - tick));
 df = pd.merge(projects_df, outcomes_df, how = 'left', on = 'projectid')
 __acc_cnt(df, list_of_vars, list_of_cnt_cols)
 
-print df[df['group'] != 'test'][['projectid', 'group', 'teacher_acctid', 'is_exciting', 'is_exciting_cnt', 'is_exciting_cnt_acc_by_teacher_acctid', 'cum_cnt_by_teacher_acctid', 'is_exciting_cnt_rate_by_teacher_acctid']].head()
-
+print df[df['group'] != 'test'][['projectid', 'group', 'teacher_acctid', 'is_exciting', 'is_exciting_cnt', 'is_exciting_cnt_acc_for_teacher_acctid', 'cum_cnt_for_teacher_acctid', 'is_exciting_cnt_rate_for_teacher_acctid']].head()
+df[df['group']=='train'][:500].to_csv('history.csv')
 
 
