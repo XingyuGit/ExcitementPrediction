0789a6e76c1c9a6e11072f4111d07b46c02a2d15
Terry Chen <TerryChen@Xuhuis-MacBook-Pro.local>
2015-03-04 04:16:59 -0800

    change to OO

--- Models/gbm_train_predict.py
+++ Models/gbm_train_predict.py
@@ -8,52 +8,56 @@ from sklearn.ensemble import GradientBoostingClassifier
 
 import import_data
 
-_features = ['projectid']
-_parameters = dict()
-_input_files = []
-_output_fn = ''
-_data_path = '../Data'
+class GBM:
+    def __init__(self, features):
+        self._features = ['projectid']
+        self._features.append(features)
+        self._parameters = dict()
 
-def set_training_features(features):
-    for x in range(len(features)):
-        _features.append(x)
 
-def set_model_parameters(number_trees, learning_rate=0.1, min_sample_split=750, loss='deviance', subsample=0.5, max_leaf_nodes=100):
-    _parameters['n_estimators'] = number_trees
-    _parameters['shrinkage'] = learning_rate
-    _parameters['min_split'] = min_sample_split
-    _parameters['loss'] = loss
-    _parameters['subsample'] = subsample
-    _parameters['max_leaf_nodes'] = max_leaf_nodes
+    def set_model_parameters(self, number_trees, learning_rate=0.1, min_sample_split=750, loss='deviance', subsample=0.5, max_leaf_nodes=100):
+        self._parameters['n_estimators'] = number_trees
+        self._parameters['shrinkage'] = learning_rate
+        self._parameters['min_split'] = min_sample_split
+        self._parameters['loss'] = loss
+        self._parameters['subsample'] = subsample
+        self._parameters['max_leaf_nodes'] = max_leaf_nodes
 
-def set_input_output_files(input_files, output_fn, data_path):
-    _input_files = input_files
-    _output_fn = output_fn
 
-def train_and_predict():
-    outcomes_df = import_data.get_outcomes_df(_data_path)
-    projects_df = import_data.get_projects_df(_data_path)
-    df = pd.merge(projects_df, outcomes_df, how='left', on='projectid')[['projectid', 'group', 'y']]
-    for x in range(len(_input_files)):
-        input_df = pd.read_csv(os.path.join('../Features_csv'), _input_files[x])
-        df = pd.merge(df, input_df, how='left', on='projectid')
+    def train_and_predict(self, input_files, output_fn, data_path='../Data'):
+        """
 
-    x_train_df = df[_features][(df['group'] == 'valid') | (df['group'] == 'train')]
-    y_train_df = df['y'][(df['group'] == 'valid') | (df['group'] == 'train')]
-    x_test_df = df[_features][df['group'] == 'test']
+        :param input_files:  a list of features files
+        :param output_fn: filename of output file (including .csv)
+        :param data_path: path of origin data
+        """
 
-    x_train_matrix = x_train_df.as_matrix()
-    y_train_matrix = y_train_df.as_matrix()
-    x_test_matrix = x_test_df.as_matrix()
+        ##  obtain complete data set for training
+        outcomes_df = import_data.get_outcomes_df(data_path)
+        projects_df = import_data.get_projects_df(data_path)
+        df = pd.merge(projects_df, outcomes_df, how='left', on='projectid')[['projectid', 'group', 'y']]
+        for x in range(len(input_files)):
+            input_df = pd.read_csv(os.path.join('../Features_csv'), self.input_files[x])
+            df = pd.merge(df, input_df, how='left', on='projectid')
 
-    clf = GradientBoostingClassifier(n_estimators=_parameters['n_estimators'], loss=_parameters['loss'], min_samples_split=_parameters['min_split'], subsample=_parameters['subsample'], max_leaf_nodes=_parameters['max_leaf_nodes'])
-    clf.fit(x_train_matrix, y_train_matrix)
+        #   split into train data and test data
+        x_train_df = df[self._features][(df['group'] == 'valid') | (df['group'] == 'train')]
+        y_train_df = df['y'][(df['group'] == 'valid') | (df['group'] == 'train')]
+        x_test_df = df[self._features][df['group'] == 'test']
 
-    y_test_matrix = clf.predict(x_test_matrix)
-    y_test_df = pd.DataFrame(y_test_matrix, columns='y')
+        #   convert to 2D array
+        x_train_matrix = x_train_df.as_matrix()
+        y_train_matrix = y_train_df.as_matrix()
+        x_test_matrix = x_test_df.as_matrix()
 
-    # write to file
-    print('writing file: ' + _output_fn)
-    y_test_df.to_csv(os.path.join('../Prediction'), _output_fn)
+        #   train model
+        clf = GradientBoostingClassifier(n_estimators=self._parameters['n_estimators'], loss=self._parameters['loss'], min_samples_split=self._parameters['min_split'], subsample=self._parameters['subsample'], max_leaf_nodes=self._parameters['max_leaf_nodes'])
+        clf.fit(x_train_matrix, y_train_matrix)
 
-    sys.exit()
+        #   predict and convert to dataframe for writing
+        y_test_matrix = clf.predict(x_test_matrix)
+        y_test_df = pd.DataFrame(y_test_matrix, columns='y')
+
+        # write to file
+        print('writing file: ' + self.output_fn)
+        y_test_df.to_csv(os.path.join('../Prediction'), self.output_fn)
